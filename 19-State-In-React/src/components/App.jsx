eec384645924db266ece6d2995039d483ee2e5bb
Md Shahil <sahil321nis@gmail.com>
2025-03-16 22:20:01 +0530

    state in react

---
+++ 19-State-In-React/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+// var isDone = false;
+var isDone = true;
+
+// function strike() {
+//   isDone = true;
+// }
+
+// function unStrike() {
+//   isDone = false;
+// }
+
+function App() {
+  return (
+    <div>
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      {/* <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button> */}
+    </div>
+  );
+}
+
+export default App;
