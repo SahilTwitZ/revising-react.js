c6dd6a63e0c35ef1171ab63636b38d27069e2510
Md Shahil <sahil321nis@gmail.com>
2025-03-23 10:25:26 +0530

    to do list

---
+++ 31-To-Do-List-2-React/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./../public/styles.css";
+import reactDom from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<App />);
